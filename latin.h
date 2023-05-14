33f3baf2694f66c894ccccef35128b62a5f6c7b8
adom44 <adomatis@hotmail.sk>
2023-05-14 18:49:20 +0200

    Cleaned up mess in dictionary

--- latin.h
+++ latin.h
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+//some terms
 #define mille 1000
 #define primus main
 #define nihil NULL
-#define redde return
-//#define reddere return
+
+//conditionals
 #define si if
 #define aliter else
-//#define facere do
 #define fac do
 #define dum while
 #define nam for
-//#define mutare switch
 #define muta switch
-//#define secare break
+
+
+//actions
 #define seca break
-//#define perservere continue
 #define perserva continue
+#define redde return
+
+//datatypes
 #define integer int
 #define signum char
 #define catena char*
 #define realis float
 #define longum double
-//#define legere(...) scanf(__VA_ARGS__)
+
+//commands
 #define lege(...) scanf(__VA_ARGS__)
-//#define scribere(...) printf(__VA_ARGS__)
 #define scribe(...) printf(__VA_ARGS__)
