7e96eb6e271a641df3d3191e33c9605f4b7858fe
adom44 <adomatis@hotmail.sk>
2023-05-19 20:42:20 +0200

    Basic map generation

--- terra-apocalipsi/terra-apocalipsi.c
+++ terra-apocalipsi/terra-apocalipsi.c
@@ -1,7 +1,23 @@
 #include "../latin.h"
+int latitudoMundi=100, longitudoMundi=50;
+
+
+vacuum MundumGenerare(){
+	per(int y=1; y<=longitudoMundi; y++){
+		per(int x=1; x<=latitudoMundi; x++){
+			//depictare margines mappae
+			si(x==1 || x==latitudoMundi || y==1 || y==longitudoMundi){
+				scribe("#");
+			}aliter{
+				scribe(" ");
+			}
+		}
+		scribe("%c",NOVA_LINEA);
+	}
+}
 
 integer primus(){
-	
+	MundumGenerare();
 	
 	redde 0;
 }
